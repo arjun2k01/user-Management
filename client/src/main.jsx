da1f1b6930bd1984d23bc02c503567a40012efab
arjun2k01 <122463451+arjun2k01@users.noreply.github.com>
2025-12-13 02:26:45 +0530

    completed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,28 +5,30 @@ import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 import App from "./App";
-import { AuthProvider } from "./AuthContext";
 import ErrorBoundary from "./components/common/ErrorBoundary";
+
+import { ThemeProvider } from "./ThemeContext";
+import { AuthProvider } from "./context/AuthContext";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <BrowserRouter>
-        <AuthProvider>
-          <App />
-          <Toaster
-            position="top-right"
-            toastOptions={{
-              duration: 3000,
-              style: {
-                fontSize: "13px",
-              },
-            }}
-          />
-        </AuthProvider>
-      </BrowserRouter>
+      <ThemeProvider>
+        <BrowserRouter>
+          <AuthProvider>
+            <App />
+            <Toaster
+              position="top-right"
+              toastOptions={{
+                duration: 3000,
+                style: { fontSize: "13px" },
+              }}
+            />
+          </AuthProvider>
+        </BrowserRouter>
+      </ThemeProvider>
     </ErrorBoundary>
   </React.StrictMode>
 );
